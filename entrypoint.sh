4ce2b2dfa5e667b00417c70bd58a7f8176e6a66b
rxgraves <125855819+rxgraves@users.noreply.github.com>
2025-11-09 03:18:04 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ if [ ! -f "$RCLONE_CONFIG_PATH" ]; then
   echo "No rclone config found. Upload via bot or mount one."
 fi
 
-echo "Bot starting at: $(date -u)"
+echo "System time (from host): $(date -u)"
+echo "Starting bot..."
 exec python /app/bot.py
